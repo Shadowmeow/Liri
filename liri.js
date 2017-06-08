a9e8bdb09ab4028abe97957d53f4e37d9420dc4b
Winston Lum <meow@Winstons-MacBook-Pro.local>
2017-06-08 17:55:25 -0400

    2nd

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var Twitter = require("twitter");
 var keys = require("keys");
 var client = new Twitter(keys.twitterKeys);
 var params = {
-    screen_name: "ryansy201",
+    screen_name: process.argv[3],
     count: 20
     }
 var request = require("request");
@@ -28,7 +28,7 @@ switch (action) {
 function tweets() {
     client.get("statuses/user_timeline", params, function(error, tweets, response) {
         if (!error && response.statusCode == 200) {
-            fs.appendFile("terminal.log", ("=============== LOG ENTRY BEGIN ===============\r\n" + Date() + "\r\n \r\nTERMINAL COMMANDS:\r\n$: " + process.argv + "\r\n \r\nDATA OUTPUT:\r\n"), function(err) {
+            fs.appendFile("terminal.log", ("=========== LOG BEGIN ===========\r\n" + Date() + "\r\n \r\nTERMINAL COMMANDS:\r\n$: " + process.argv + "\r\n \r\nDATA OUTPUT:\r\n"), function(err) {
                 if (err) throw err;
             });
             console.log(" ");
@@ -39,11 +39,13 @@ function tweets() {
                 console.log([i + 1] + ". " + tweets[i].text);
                 console.log("Created on: " + tweets[i].created_at);
                 console.log(" ");
-                fs.appendFile("terminal.log", (number + ". Tweet: " + tweets[i].text + "\r\nCreated at: " + tweets[i].created_at + " \r\n"), function(err) {
+                fs.appendFile("terminal.log", (number + ". Tweet: " + tweets[i].text
+                 + "\r\nCreated at: " + tweets[i].created_at + " \r\n"), function(err) {
                     if (err) throw err;
                 });
             }
-            fs.appendFile("terminal.log", ("=============== LOG ENTRY END ===============\r\n \r\n"), function(err) {
+            fs.appendFile("terminal.log", ("=========== LOG END ===========\r\n \r\n")
+                , function(err) {
                 if (err) throw err;
             });
         }
@@ -63,7 +65,7 @@ function spotifyThis(value) {
             console.log("Preview Link: " + jsonBody.tracks.items[0].preview_url);
             console.log("Album: " + jsonBody.tracks.items[0].album.name);
             console.log(" ");
-            fs.appendFile("terminal.log", ("=============== LOG ENTRY BEGIN ===============\r\n" + Date() +"\r\n \r\nTERMINAL COMMANDS:\r\n$: " + process.argv + "\r\n \r\nDATA OUTPUT:\r\n" + "Artist: " + jsonBody.tracks.items[0].artists[0].name + "\r\nSong: " + jsonBody.tracks.items[0].name + "\r\nPreview Link: " + jsonBody.tracks.items[0].preview_url + "\r\nAlbum: " + jsonBody.tracks.items[0].album.name + "\r\n=============== LOG ENTRY END ===============\r\n \r\n"), function(err) {
+            fs.appendFile("terminal.log", ("=========== LOG ENTRY BEGIN ===========\r\n" + Date() +"\r\n \r\nTERMINAL COMMANDS:\r\n$: " + process.argv + "\r\n \r\nDATA OUTPUT:\r\n" + "Artist: " + jsonBody.tracks.items[0].artists[0].name + "\r\nSong: " + jsonBody.tracks.items[0].name + "\r\nPreview Link: " + jsonBody.tracks.items[0].preview_url + "\r\nAlbum: " + jsonBody.tracks.items[0].album.name + "\r\n=============== LOG ENTRY END ===============\r\n \r\n"), function(err) {
                 if (err) throw err;
             });
         }
@@ -71,7 +73,7 @@ function spotifyThis(value) {
 }
 
 function movieThis(value) {
-    if (value == null) {
+    if (value === null) {
         value = "wargames";
     }
     request("http://www.omdbapi.com/?t=" + value + "&tomatoes=true&r=json", function(error, response, body) {
